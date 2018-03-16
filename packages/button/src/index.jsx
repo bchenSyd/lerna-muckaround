312a54a970d81785715c4f520482c0db26d52827
Bo Chen <bochen2014@yahoo.com>
2018-03-16 14:23:51 +1100

    update

---
+++ packages/button/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import getName from './utils/getName';
+
+const  FancyButton = ()=><button>fancy button ${getName}</button>
+
+export default FancyButton;
---
+++ packages/button/src/utils/getName.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+
+
+export default function getName() {
+    return _.times(3).reduce((acc, val) => acc + ' ' + val);
+}
