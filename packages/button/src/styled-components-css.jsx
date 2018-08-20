3d207d14350401c95b072d8e6008792a8f59effd
Bo Chen <bo.chen@westpac.com.au>
2018-08-20 14:43:55 +1000

    styled components

--- packages/button/src/styled-components-css.jsx
+++ packages/button/src/styled-components-css.jsx
@@ -3,19 +3,21 @@ import { render } from "react-dom";
 import styled, { css } from "styled-components";
 import Decoarated from "./hello-wrapper";
 
-
 // https://codesandbox.io/s/48j6v915k0
 const MyComponent = styled("div")`
   h1 {
-    color: red
+    color: red;
   }
 
- ${({ bgColor }) =>
-   bgColor &&
-   css`
-  &:hover{
-    background-color: ${bgColor};
-  }`} 
+  ${({ bgColor }) =>
+    bgColor &&
+    // 1. if we were not using ${bgColor}, we could just put it under a normal string; see example below
+    // 2. looks like in this example, if we remove `css` but keep back tilt `, it still works
+    css`
+      &:hover {
+        background-color: ${bgColor};
+      }
+    `} ${({ bgColor }) => bgColor && "&:hover{ border: 1px solid grey}"};
 `;
 
 class App extends Component {
