1f4546237a4ea8afbff0085abc41917d21f4e8bd
Bo Chen <bo.chen@westpac.com.au>
2018-08-20 12:08:31 +1000

    grid-styled

---
+++ packages/button/src/grid-styled.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Flex, Box} from 'grid-styled';
+
+
+const GridStyledComponents = () => {
+    return (
+        // https://github.com/jxnblk/grid-styled#changing-the-html-element
+        <Box is="header" >
+             {/*https://github.com/jxnblk/grid-styled/issues/141    with has same effect as padding-right */}
+             
+             <Box width={[1,1/2]}>hello</Box>
+             <Box width={[1,1/2]}>world</Box>
+        </Box>
+    );
+};
+
+export default GridStyledComponents;
