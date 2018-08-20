3bb1277d8e9c87d0a87aee87a2cb51819ef512c7
Bo Chen <bo.chen@westpac.com.au>
2018-08-20 15:07:31 +1000

    styled components

--- packages/button/src/grid-styled.js
+++ packages/button/src/grid-styled.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import {Flex, Box} from 'grid-styled';
-
+import React from "react";
+import { Flex, Box } from "grid-styled";
 
 const GridStyledComponents = () => {
-    return (
-        // https://github.com/jxnblk/grid-styled#changing-the-html-element
-        <Box is="header" >
-             {/*https://github.com/jxnblk/grid-styled/issues/141    with has same effect as padding-right */}
-             
-             <Box width={[1,1/2]}>hello</Box>
-             <Box width={[1,1/2]}>world</Box>
-        </Box>
-    );
+  return (
+   // 0-xs  column; xs-sm  column; md and lg row;  so effectively, this is max-width;
+    <Flex direction={["column", "column", "row"]}>
+      // https://github.com/jxnblk/grid-styled#changing-the-html-element
+      <Box is="header">
+        {/*https://github.com/jxnblk/grid-styled/issues/141    with has same effect as padding-right */}
+
+        <Box width={[1, 1 / 2]}>hello</Box>
+        <Box width={[1, 1 / 2]}>world</Box>
+      </Box>
+    </Flex>
+  );
 };
-
-
 
 export default GridStyledComponents;
