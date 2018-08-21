47670b9067f4aaf3ca6d05b601192833ee9e3e38
Bo Chen <bo.chen@westpac.com.au>
2018-08-21 12:44:32 +1000

    update

--- packages/button/src/Th.styles.js
+++ packages/button/src/Th.styles.js
@@ -17,8 +17,19 @@ export const StyledTh = styled.th.attrs({
   className: ({ highlighted }) => (highlighted ? classHighlighted : null),
 })`
   text-align: left;
+  /* when I'm a child of [ STyledThead > STyledTr ] > & { my styles goes here}
 
+   // https://codesandbox.io/s/48j6v915k0   the '&' is required in this case
+    const Header = styled.h1`
+        ${ParentComponent} > & {
+            color: red;
+        }
+        ${ParentComponent}:hover & {
 
+        }
+    `
+  
+  */
   ${StyledThead} > ${StyledTr} > &,
     ${StyledTbody} > ${StyledTr} > &,
     ${StyledTfoot} > ${StyledTr} > & {
@@ -31,20 +42,6 @@ export const StyledTh = styled.th.attrs({
     }) => color.border.default};
   }
 
-  ${StyledThead} > ${StyledTr} > & {
-    vertical-align: bottom;
-    border-bottom: 3px solid ${({
-      theme: {
-        table: { color },
-      },
-    }) => color.border.th};
-    font-weight: 500;
-    color: ${({
-      theme: {
-        table: { color },
-      },
-    }) => color.font.th};
-  }
 
   ${StyledCaption} + ${StyledThead} > ${StyledTr}:first-child > &,
     ${StyledColgroup} + ${StyledThead} > ${StyledTr}:first-child > &,
@@ -61,102 +58,6 @@ export const StyledTh = styled.th.attrs({
             table: { color },
           },
         }) => color.border.highlight};
-      }
-    `};
-
-  ${({ highlighted }) =>
-    highlighted &&
-    css`
-      border-left: 6px solid
-        ${({
-          theme: {
-            table: { color },
-          },
-        }) => color.border.highlight};
-      border-bottom: 1px solid
-        ${({
-          theme: {
-            table: { color },
-          },
-        }) => color.border.highlight};
-
-      &.${classHighlighted} + & {
-        border-left: 0 none;
-      }
-    `};
-
-  ${({ bordered }) =>
-    bordered &&
-    css`
-      border-left: 1px solid ${({
-        theme: {
-          table: { color },
-        },
-      }) => color.border.default};
-      border-right: 1px solid ${({
-        theme: {
-          table: { color },
-        },
-      }) => color.border.default};
-
-      ${StyledCaption} + ${StyledThead} > ${StyledTr}:first-child > & {
-        border-top: 1px solid ${({
-          theme: {
-            table: { color },
-          },
-        }) => color.border.default};
-      }
-
-      ${StyledThead} > ${StyledTr} > & {
-        border-bottom-width: 2px;
-      }
-    `};
-
-  ${({ highlighted, bordered }) =>
-    highlighted &&
-    bordered &&
-    css`
-      ${StyledTbody} > ${StyledTr} > & + & {
-        border-left: 1px solid ${({
-          theme: {
-            table: { color },
-          },
-        }) => color.border.default};
-      }
-    `};
-
-  ${({ theme, responsive }) =>
-    responsive &&
-    theme.breakpointMax.xs`
-      white-space: nowrap;
-    `};
-
-  ${({ theme, responsive, highlighted }) =>
-    responsive &&
-    highlighted &&
-    theme.breakpointMax.xs`
-      &:first-child {
-        border-left: 6px solid ${({
-          theme: {
-            table: { color },
-          },
-        }) => color.border.highlight};
-      }
-    `};
-
-  ${({ theme, responsive, bordered }) =>
-    responsive &&
-    bordered &&
-    theme.breakpointMax.xs`
-      &:first-child {
-        border-left: 0;
-      }
-      &:last-child {
-        border-right: 0;
-      }
-
-      ${StyledTr}:last-child > & {
-        border-bottom: 0;
       }
     `};
 `;
