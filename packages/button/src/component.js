5cbc72a578ae71af981200cfe0928b15e11af8eb
Bo Chen <bo.chen@westpac.com.au>
2018-10-25 14:17:15 +1100

    update

--- packages/button/src/component.js
+++ packages/button/src/component.js
@@ -9,6 +9,7 @@ export const MyComponent = styled(({ className, name, ...rest }) => (
   font-size: 2.5em;
 `;
 
+// extend keyword is deprecated and has already been removed in ver 4
 const extendedComponent = MyComponent.extend`
   color:'must be css prop ONLY';
 `
@@ -34,20 +35,21 @@ const HighlightText = styled(Box).attrs({ size: 2 })`
 // attrs can update any props
 // note: all extends/attrs rely on component being wrapped passing on props (className and ...restProps)
 // down to leaf DOM elements )
-const Decorated = MyComponent.extend.attrs({
+const Decorated = styled(MyComponent).attrs({
   type: "password", // ...resProps change;
-})``; //  you need the two backticks at the end even if you don't want to override the styles; or you can use `recompose`
+})``; //  you need the two backticks at the end even if you don't want to override the styles
 
 
-const Exception = styled.a.attrs({ target: "_blank" })``;
+// see https://github.com/styled-components/styled-components/issues/1250#issuecomment-337181485
 
 // btw, guys don't you think that this is not that obvious that attrs mutate given component instead cloning it?
 // I have not found in documentation that attrs should be used with extend
-// only exception being :
+// only exception being the component being extended is a html primitive one:
+
+const Exception = styled.a.attrs({ target: "_blank" })``;
 
 // you can't do YourComponent.attrs({prop: value}) as once a Component is defined and being used, it's very dangerouse to modify it
 // however, you can do styled(YourComponent).attrs({prop:'value})`` , or YourComponent.extend.attrs({})``
 // that's why `.extend` comes to play,
-// see https://github.com/styled-components/styled-components/issues/1250#issuecomment-337181485
 
 export default Decorated;
